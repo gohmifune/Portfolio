66d2f6a1c06a57eb1aeefea7b8249409faf196ee
Christopher Wilson <cwilson55@my.apsu.edu>
2012-11-30 17:11:58 -0600

    Initial Push

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+	<meta charset="utf-8" />
+	<title>Christopher Wilson: Portfolio Website</title>
+	
+	<link rel="stylesheet" type="text/css" href="../css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="../css/desktop.css" />
+</head>
+
+<body>
+	<div id="container">
+		<div id="profile" class="window">
+		
+		</div>
+		
+		<div id="click" class="window">
+		
+		</div>
+	</div>
+	
+</body>
+</html>
---
+++ js/calculator.js
@@ -0,0 +1,34 @@
+//This program is to make a calculator. Nothin complicated.
+//Christopher Wilson
+
+/* Program walkthrough
+   User clicks a number button
+   Number appears on screen
+   clicks math function, function key glows
+   User clicks second number key, function key stops glowing
+   User clicks math function, function key glows
+   User clicks mem store key, mem store key glows
+   User clicks clear key, total is erased screen disappears
+   User clicks mem-restore key, mem-store key returns to normal
+   User clicks math function, function key glows
+   User clicks another number key, function key stops glowing
+   User clicks equal
+   Total appears on screen
+ */
+
+/* Program features:
+ * Buttons should change color when clicked
+ * Numbers should be sent to view window
+ * Capable of:
+      * Add
+      * Subtract
+      * Multiply
+      * Divide
+      * Clear
+      * Memory store/restore
+ * Variables neded:
+      * number
+      * total
+      * memory
+      * calculation
+ */
